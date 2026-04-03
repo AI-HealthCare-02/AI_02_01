33afa252dd09bc96eea951e5f47027ba0893b7f8
yysop <sysysy3142@gmail.com>
2026-04-03 11:53:18 +0900

    [FE] chore: React Router 및 앱 진입 구조 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,22 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/Landing/LandingPage";
 import LoginPage from "./pages/Login/LoginPage";
+import DashboardPage from "./pages/Dashboard/DashboardPage";
+import ChallengePage from "./pages/Challenge/ChallengePage";
+import HealthInputPage from "./pages/HealthInput/HealthInputPage";
+import ResultPage from "./pages/Result/ResultPage";
 
 function App() {
-  return <LoginPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/health-input" element={<HealthInputPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/challenge" element={<ChallengePage />} />
+      <Route path="/result" element={<ResultPage />} />
+    </Routes>
+  );
 }
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
 import "./index.css";
 
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={clientId}>
-      <App />
+    <GoogleOAuthProvider clientId="여기에_구글_클라이언트_ID">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
