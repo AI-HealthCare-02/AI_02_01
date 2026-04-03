2e999bce737910aa7bea778ef1c758b1d619a618
yysop <sysysy3142@gmail.com>
2026-04-03 20:22:46 +0900

    [FE] refactor: 앱 라우팅 및 엔트리 구조 정리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/Landing/LandingPage";
 import LoginPage from "./pages/Login/LoginPage";
-import DashboardPage from "./pages/Dashboard/DashboardPage";
-import ChallengePage from "./pages/Challenge/ChallengePage";
 import HealthInputPage from "./pages/HealthInput/HealthInputPage";
 import ResultPage from "./pages/Result/ResultPage";
+import DashboardPage from "./pages/Dashboard/DashboardPage";
+import ChallengePage from "./pages/Challenge/ChallengePage";
 
 function App() {
   return (
@@ -12,9 +12,9 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/health-input" element={<HealthInputPage />} />
+      <Route path="/result" element={<ResultPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
       <Route path="/challenge" element={<ChallengePage />} />
-      <Route path="/result" element={<ResultPage />} />
     </Routes>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
-import "./index.css";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="여기에_구글_클라이언트_ID">
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </GoogleOAuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
