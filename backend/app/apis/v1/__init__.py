69ed0d416b87946021da14c630fbec629a39c809
이형석 <gudtjr1574@gmail.com>
2026-04-22 14:43:52 +0900

    [BE] feat: 건강검진 OCR 라우터 추가 및 라우터 등록

--- backend/app/apis/v1/__init__.py
+++ backend/app/apis/v1/__init__.py
@@ -7,6 +7,7 @@ from app.apis.v1.social_routers import social_router
 from app.apis.v1.user_routers import user_router
 from app.apis.v1.ai_vision_routers import ai_vision_router
 from app.apis.v1.challenge_routers import challenge_router, user_challenge_router
+from app.apis.v1.checkup_routers import checkup_router
 
 v1_routers = APIRouter(prefix="/api/v1")
 v1_routers.include_router(analysis_router)
@@ -17,3 +18,4 @@ v1_routers.include_router(user_router)
 v1_routers.include_router(ai_vision_router)
 v1_routers.include_router(challenge_router)
 v1_routers.include_router(user_challenge_router)
+v1_routers.include_router(checkup_router)
